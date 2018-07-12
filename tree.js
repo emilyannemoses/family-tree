a241dee9c8b236ca7493c3eb9fb4c4c1eaa5258e
Emily Anne Moses <e.a.moses@gmail.com>
2018-07-11 20:35:30 -0500

    Saving duplication process

---
+++ tree.js
@@ -0,0 +1,9 @@
+var i = 0;
+
+function duplicate() {
+    var original = document.getElementById('duplicater' + i);
+    var clone = original.cloneNode(true); // "deep" clone
+    clone.id = "duplicater" + ++i; // there can only be one element with an ID
+    clone.onclick = duplicate; // event handlers are not cloned
+    original.parentNode.appendChild(clone);
+}
